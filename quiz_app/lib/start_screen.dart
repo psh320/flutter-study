e9f2030203341ab6193cae0d1933b904bb36276d
Sungho Park <parksungho320@gmail.com>
2025-03-01 16:26:57 +0800

    add button and opacity

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -5,6 +5,33 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Center(child: Text('Start Screen'));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset('assets/images/quiz-logo.png', width: 300),
+          // ),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(height: 40),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            label: const Text('Start Quiz'),
+            icon: const Icon(Icons.arrow_right_alt, color: Colors.white),
+          ),
+        ],
+      ),
+    );
   }
 }
